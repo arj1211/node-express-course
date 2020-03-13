170c3e1d9b0f530b80d52181ffc66d29e03e26f4
arj1211 <arjun.bawa@hotmail.com>
2020-03-13 19:25:15 -0400

    added dynamic GET route

--- server.js
+++ server.js
@@ -11,4 +11,12 @@ app.get('/users', (req, res) => {
         users: mockUserData
     })
 });
+app.get('/users/:id', (req, res) => {
+    console.log(`got user ${req.params.id}!`);
+    res.json({
+        success: true,
+    message: `got user ${req.params.id}!`,
+        users: req.params.id
+    })
+});
 app.listen(8000, () => { console.log('server is running'); })
